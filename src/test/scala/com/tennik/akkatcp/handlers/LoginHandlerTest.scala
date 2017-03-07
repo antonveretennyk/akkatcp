54c415e0662569b5c15d2f5220494caa7a89589e
Anton Veretennik <antonv@wix.com>
2017-03-08 00:38:55 +0200

    Extract fixtures for LoginHandlerTest

--- src/test/scala/com/tennik/akkatcp/handlers/LoginHandlerTest.scala
+++ src/test/scala/com/tennik/akkatcp/handlers/LoginHandlerTest.scala
@@ -1,31 +1,30 @@
 package com.tennik.akkatcp.handlers
 
-import akka.actor.{ ActorSystem, Props }
-import akka.testkit.{ ImplicitSender, TestActorRef, TestKit }
+import akka.actor.{ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
 import akka.util.ByteString
-import com.tennik.akkatcp.dao.{ InMemoryUserDAO, UserDao }
-import com.tennik.akkatcp.protobuf.test.{ AutorizationRequest, AutorizationResponse, ERROR_CODE }
-import org.scalatest.{ BeforeAndAfterAll, Matchers, WordSpecLike }
+import com.tennik.akkatcp.dao.{InMemoryUserDAO, UserDao}
+import com.tennik.akkatcp.protobuf.test.{AutorizationRequest, AutorizationResponse, ERROR_CODE}
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
 import scala.concurrent.duration._
 
 class LoginHandlerTest() extends TestKit(ActorSystem("LoginHandlerSystem"))
-    with ImplicitSender with WordSpecLike with Matchers with BeforeAndAfterAll {
+  with ImplicitSender with WordSpecLike with Matchers with BeforeAndAfterAll {
 
   trait Context {
-    val sut = TestActorRef(Props(new LoginHandler(userDAO = userDao)), "Hndlr")
+    val userDao = new InMemoryUserDAO
+    userDao.init()
+    val sut = TestActorRef(Props(new LoginHandler(userDAO = userDao)))
+
+    def payload(cnt: Int) = ByteString(AutorizationResponse(ERROR_CODE.OK, Option(cnt)).toByteArray)
+
+    def adminAuthRequest = ByteString(AutorizationRequest.toByteArray(AutorizationRequest("admin", "password")))
+
+    def user1AuthRequest = ByteString(AutorizationRequest.toByteArray(AutorizationRequest("user1", "password1")))
   }
 
   import akka.io.Tcp._
-
-  val counter = 1
-  def payload(cnt: Int) = ByteString(AutorizationResponse(ERROR_CODE.OK, Option(cnt)).toByteArray)
-
-  def adminAuthRequest = ByteString(AutorizationRequest.toByteArray(AutorizationRequest("admin", "password")))
-  def user1AuthRequest = ByteString(AutorizationRequest.toByteArray(AutorizationRequest("user1", "password1")))
-
-  val userDao = new InMemoryUserDAO
-  userDao.init()
 
   override def afterAll {
     TestKit.shutdownActorSystem(system)
@@ -39,8 +38,7 @@ class LoginHandlerTest() extends TestKit(ActorSystem("LoginHandlerSystem"))
       expectMsg(Write(payload(1)))
     }
 
-    "respose with counter == 2 for 2 received messages" in {
-      val sut = TestActorRef(Props(new LoginHandler(userDAO = userDao)), "DoubleHndlr")
+    "respose with counter == 2 for 2 received messages" in new Context {
       within(500 millis) {
         sut ! Received(adminAuthRequest)
         expectMsg(Write(payload(1)))
@@ -50,8 +48,7 @@ class LoginHandlerTest() extends TestKit(ActorSystem("LoginHandlerSystem"))
       }
     }
 
-    "response with counter1 == 2, counter2 =1 in case of requests for different users" in {
-      val sut = TestActorRef(Props(new LoginHandler(userDAO = userDao)), "TripleHndlr")
+    "response with counter1 == 2, counter2 =1 in case of requests for different users" in new Context {
       within(500 millis) {
         sut ! Received(adminAuthRequest)
         expectMsg(Write(payload(1)))
@@ -61,7 +58,6 @@ class LoginHandlerTest() extends TestKit(ActorSystem("LoginHandlerSystem"))
 
         sut ! Received(adminAuthRequest)
         expectMsg(Write(payload(2)))
-
       }
     }
   }
